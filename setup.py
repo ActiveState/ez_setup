732e033832291e03735b54cab68d4864d71eb276
Sridhar Ratnakumar <me@srid.name>
2010-11-22 15:58:24 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.9'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='ez_setup',
+    version=version,
+    description="ez_setup.py and distribute_setup.py",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='ez_setup setuptools distribute workaround',
+    author='Sridhar Ratnakumar',
+    author_email='github@srid.name',
+    url='http://github.com/ActiveState/ez_setup',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['ez_setup=ez_setup:main']
+    }
+)
